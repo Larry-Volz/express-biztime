b6be5f9382f28603415f78c23a9a4bbfa73ed591
Larry Volz <imaginologist@gmail.com>
2021-11-18 21:31:49 -0500

    all routes done for company

--- express-biztime/routes/companies.js
+++ express-biztime/routes/companies.js
@@ -1,11 +1,12 @@
 const db = require("../db");
 const express = require("express");
+const expressError = require("../expressError");
 const router = express.Router();
 
 router.get("/", async function (req, res, next) {
     try {
       const results = await db.query(
-            `SELECT code, name 
+            `SELECT code, name
             FROM companies 
             ORDER BY name`);
   
@@ -29,7 +30,7 @@ try{
 } catch(err) {
     return next(err);
 }
-})
+});
 
 
 router.post("/", async function(req, res, next){
@@ -45,6 +46,49 @@ try{
     return next(err);
 }
 })
+
+
+router.put("/:code", async function(req, res, next){
+
+    try{
+        const code = req.params.code;
+        const { name, description } = req.body;
+
+        const results = await db.query(
+            `UPDATE companies SET name=$1, description=$2 WHERE code = $3 RETURNING code, name, description`, [name, description, code]
+        );
+
+        if (results.rows.length == 0) {
+            throw new expressError(`${code} COMPANY NOT FOUND`, 404)
+          } else {
+            return res.json({"company": results.rows[0]});
+          }
+    
+    } catch(err) {
+        return next(err);
+    }
+    });
+
+    router.delete("/:code", async function(req, res, next){
+
+        try{
+            const code = req.params.code;
+            const results = await db.query(
+                `DELETE FROM companies 
+                WHERE code = $1
+                RETURNING code`, [code]
+            )
+            
+            if (results.rows.length == 0) {
+                throw new expressError(`${code} COMPANY NOT FOUND`, 404)
+              } else {
+                return res.json({"status": `${code} DELETED`});
+              }
+        
+        } catch(err) {
+            return next(err);
+        }
+        });
 
 
 module.exports = router;
