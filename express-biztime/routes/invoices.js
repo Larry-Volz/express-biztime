a909792054b656a4253e789866f2ae670264fcb0
Larry Volz <imaginologist@gmail.com>
2021-11-18 22:21:43 -0500

    all but last route done

--- express-biztime/routes/invoices.js
+++ express-biztime/routes/invoices.js
@@ -59,20 +59,22 @@ try{
 })
 
 
-router.put("/:code", async function(req, res, next){
+router.put("/:id", async function(req, res, next){
 
     try{
-        const code = req.params.code;
-        const { name, description } = req.body;
+        const { id }  = req.params;
+        const { amt } = req.body;
 
         const results = await db.query(
-            `UPDATE invoices SET name=$1, description=$2 WHERE code = $3 RETURNING code, name, description`, [name, description, code]
+            `UPDATE invoices 
+            SET amt=$1 WHERE id = $2 
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`, [amt, id]
         );
 
         if (results.rows.length == 0) {
             throw new expressError(`${code} COMPANY NOT FOUND`, 404)
           } else {
-            return res.json({"company": results.rows[0]});
+            return res.json({"invoice": results.rows[0]});
           }
     
     } catch(err) {
@@ -80,20 +82,20 @@ router.put("/:code", async function(req, res, next){
     }
     });
 
-    router.delete("/:code", async function(req, res, next){
+    router.delete("/:id", async function(req, res, next){
 
         try{
-            const code = req.params.code;
+            const { id } = req.params;
             const results = await db.query(
                 `DELETE FROM invoices 
-                WHERE code = $1
-                RETURNING code`, [code]
+                WHERE id = $1
+                RETURNING id`, [id]
             )
             
             if (results.rows.length == 0) {
-                throw new expressError(`${code} COMPANY NOT FOUND`, 404)
+                throw new expressError(`INVOICE ${id} NOT FOUND`, 404)
               } else {
-                return res.json({"status": `${code} DELETED`});
+                return res.json({"INVOICE": `${id} DELETED`});
               }
         
         } catch(err) {
