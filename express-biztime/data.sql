2830777afabf1723c5ab4d0f49a0e9a6875bc126
Larry Volz <imaginologist@gmail.com>
2021-11-21 23:53:20 -0500

    seeded industries and industry_company data

--- express-biztime/data.sql
+++ express-biztime/data.sql
@@ -1,12 +1,23 @@
-\c biztime
-
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS industry_company;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
     name text NOT NULL UNIQUE,
     description text
+);
+
+CREATE TABLE industries (
+    code text PRIMARY KEY,
+    industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE industry_company (
+    id serial PRIMARY KEY,
+    indust_code text NOT NULL,
+    comp_code text NOT NULL
 );
 
 CREATE TABLE invoices (
@@ -22,6 +33,18 @@ CREATE TABLE invoices (
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
          ('ibm', 'IBM', 'Big blue.');
+
+INSERT INTO industries (code, industry)
+  VALUES ('act','accounting'),
+         ('ent','entertainment'),
+         ('hr','human resources');
+
+INSERT INTO industry_company (indust_code, comp_code)
+  VALUES ('act','apple'),
+         ('act','ibm'),
+         ('ent', 'ibm'),
+         ('hr','apple'),
+         ('hr','ibm');
 
 INSERT INTO invoices (comp_Code, amt, paid, paid_date)
   VALUES ('apple', 100, false, null),
---
+++ express-biztime/readme.md
@@ -0,0 +1,7 @@
+# Thoughts
+## Things to test:
+
+- make sure non-number for amt is screened out to prevent error
+- make sure amount is non-zero if paid
+- allow zero only if unpaid
+- 
